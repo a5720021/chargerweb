ba7d4dab547e97546ed69d52aee040c42a51f4fa
nattapong5720187 <a5720187@gmail.com>
2018-03-23 01:08:59 +0700

    check email and pass

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 // firebase.auth().onAuthStateChanged(function (user) {
 //     if (user) {
 //         // User is signed in.
 //         document.getElementById("logout_div").style.display = "block";
 //         document.getElementById("login_div").style.display = "none";
 //     }
-=======
-// firebase.auth().onAuthStateChanged(function(user) {
-//     if (user) {
-//       // User is signed in.
-//       document.getElementById("logout_div").style.display = "block";
-//       document.getElementById("login_div").style.display = "none";
-//     } 
->>>>>>> f098e1f93707c724d7c1c04c94c7d1aba7f349f7
 
 //     if (user != null) {
 //         var emailID = user.email;
 //         document.getElementById("user_id").innerHTML = "Welcome user : " + emailID;
-<<<<<<< HEAD
 //     }
 
 //     else {
@@ -29,27 +19,12 @@
 
 // function login() {
 //     // var rootRef = firebase.database().ref("ocpp-database");
-=======
-//       }
-    
-//     else {
-//       // No user is signed in.
-//       document.getElementById("logout_div").style.display = "none";
-//       document.getElementById("login_div").style.display = "block";
-//     }
-//   });
-  
-// function login(){
-//     var userEmail = document.getElementById("email_user").value;
-//     var userPass = document.getElementById("password_user").value;
->>>>>>> f098e1f93707c724d7c1c04c94c7d1aba7f349f7
 
 //     // rootRef.on("value", snap => {
 //     //     var name = snap.child("name").val();
 //     //     var pass = snap.child("password").val();
 //     // });
 
-<<<<<<< HEAD
 //     var userName = document.getElementById("name_user").value;
 //     var userPass = document.getElementById("password_user").value;
 
@@ -91,17 +66,3 @@
 //     var name = snap.child("name").val();
 //     var pass = snap.child("password").val();
 // });
-=======
-//     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // ...
-//         window.alert("Error : " + errorMessage);
-//       });
-// }
-
-// function logout(){
-//     firebase.auth().signOut();
-// }
->>>>>>> f098e1f93707c724d7c1c04c94c7d1aba7f349f7
